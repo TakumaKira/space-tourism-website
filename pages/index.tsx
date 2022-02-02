d686a0d46301de49ce317c3874a5400acd77b926
Takuma Kira <takuma@kirakiraworx.com>
2022-02-02 23:21:17 +0900

    [dev] Copy CSS from figma

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,97 +1,334 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
 import styled from 'styled-components'
+import config from '../config.json'
 
 const pre = 'SO, YOU WANT TO TRAVEL TO'
 const title = 'SPACE'
 const description = 'Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!'
 
+const {
+  responsiveBreakPointWidth: {
+    desktopToTablet,
+    tabletToMobile,
+  },
+} = config;
+
 const TextBox = styled.div`
-  width: 450px;
-  position: absolute;
-  left: 165px;
-  bottom: 113px;
+  @media (max-width: ${tabletToMobile}px) {
+    position: absolute;
+    width: 327px;
+    height: 276px;
+    left: 24px;
+    top: 112px;
+  }
+  @media (min-width: ${tabletToMobile}px) and (max-width: ${desktopToTablet}px) {
+    position: absolute;
+    width: 450px;
+    height: 334px;
+    left: 159px;
+    top: 202px;
+  }
+  @media (min-width: ${desktopToTablet}px) {
+    position: absolute;
+    width: 450px;
+    height: 382px;
+    left: 165px;
+    top: 387px;
+  }
 `
 const H2 = styled.h2`
-  font-size: 28px;
-  font-weight: normal;
-  letter-spacing: 4.725px;
-  color: #D0D6F9;
+  margin-block-start: 0;
+  margin-block-end: 0;
+  white-space: nowrap;
+  @media (max-width: ${tabletToMobile}px) {
+    position: absolute;
+    height: 19px;
+    left: 16.82%;
+    right: 16.82%;
+    top: calc(50% - 19px/2 - 128.5px);
+
+    font-family: Barlow Condensed;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 19px;
+    text-align: center;
+    letter-spacing: 2.7px;
+
+    color: #D0D6F9;
+  }
+  @media (min-width: ${tabletToMobile}px) and (max-width: ${desktopToTablet}px) {
+    position: absolute;
+    height: 24px;
+    left: 20%;
+    right: 19.78%;
+    top: calc(50% - 24px/2 - 155px);
+
+    font-family: Barlow Condensed;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 24px;
+    /* identical to box height */
+
+    text-align: center;
+    letter-spacing: 3.375px;
+
+    color: #D0D6F9;
+  }
+  @media (min-width: ${desktopToTablet}px) {
+    position: absolute;
+    height: 34px;
+    left: 0.11%;
+    right: 15.67%;
+    top: calc(50% - 34px/2 - 174px);
+
+    font-family: Barlow Condensed;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 28px;
+    line-height: 34px;
+    /* identical to box height */
+
+    letter-spacing: 4.725px;
+
+    color: #D0D6F9;
+  }
 `
 const H1 = styled.h1`
-  font-size: 150px;
-  font-weight: normal;
-  margin-block-end: 24px;
-  margin-block-start: 24px;
+  margin-block-start: 0;
+  margin-block-end: 0;
+  @media (max-width: ${tabletToMobile}px) {
+    position: absolute;
+    height: 100px;
+    left: 0%;
+    right: 0%;
+    top: calc(50% - 100px/2 - 53px);
+
+    font-family: Bellefair;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 80px;
+    line-height: 100px;
+    /* identical to box height, or 125% */
+
+    text-align: center;
+
+    color: #FFFFFF;
+  }
+  @media (min-width: ${tabletToMobile}px) and (max-width: ${desktopToTablet}px) {
+    position: absolute;
+    height: 150px;
+    left: 0%;
+    right: 1.11%;
+    top: calc(50% - 150px/2 - 44px);
+
+    font-family: Bellefair;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 150px;
+    line-height: 150px;
+    /* identical to box height, or 100% */
+
+
+    color: #FFFFFF;
+  }
+  @media (min-width: ${desktopToTablet}px) {
+    position: absolute;
+    height: 172px;
+    left: 0%;
+    right: 1.11%;
+    top: calc(50% - 172px/2 - 47px);
+
+    font-family: Bellefair;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 150px;
+    line-height: 172px;
+    /* identical to box height */
+
+
+    color: #FFFFFF;
+  }
 `
 const P = styled.p`
-  font-size: 18px;
-  line-height: 32px;
-  color: #D0D6F9;
+  margin-block-start: 0;
+  margin-block-end: 0;
+  @media (max-width: ${tabletToMobile}px) {
+    position: absolute;
+    height: 125px;
+    left: 0%;
+    right: 0%;
+    top: calc(50% - 125px/2 + 75.5px);
+
+    font-family: Barlow;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 15px;
+    line-height: 25px;
+    /* or 167% */
+
+    text-align: center;
+
+    color: #D0D6F9;
+  }
+  @media (min-width: ${tabletToMobile}px) and (max-width: ${desktopToTablet}px) {
+    position: absolute;
+    height: 112px;
+    left: 0.67%;
+    right: 0.67%;
+    top: calc(50% - 112px/2 + 111px);
+
+    font-family: Barlow;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 28px;
+    /* or 175% */
+
+    text-align: center;
+
+    color: #D0D6F9;
+  }
+  @media (min-width: ${desktopToTablet}px) {
+    position: absolute;
+    height: 128px;
+    left: 0.11%;
+    right: 1.22%;
+    top: calc(50% - 128px/2 + 127px);
+
+    font-family: Barlow;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 32px;
+    /* or 178% */
+
+
+    color: #D0D6F9;
+  }
 `
 
+const CircleContainer = styled.div`
+  @media (max-width: ${tabletToMobile}px) {
+    position: absolute;
+    width: 150px;
+    height: 150px;
+    left: 113px;
+    top: 469px;
+  }
+  @media (min-width: ${tabletToMobile}px) and (max-width: ${desktopToTablet}px) {
+    position: absolute;
+    width: 242px;
+    height: 242px;
+    left: 263px;
+    top: 692px;
+  }
+  @media (min-width: ${desktopToTablet}px) {
+    position: absolute;
+    width: 274px;
+    height: 274px;
+    left: 1001px;
+    top: 495px;
+  }
+`
 const Circle = styled.div`
-  height: 274px;
-  width: 274px;
   position: absolute;
-  right: 165px;
-  bottom: 131px;
+  height: 100%;
+  width: 100%;
   background-color: #FFFFFF;
   border-radius: 50%;
 `
-
 const CircleTransparent = styled.div`
-  height: 274px;
-  width: 274px;
   position: absolute;
-  right: 165px;
-  bottom: 131px;
+  height: 100%;
+  width: 100%;
   background-color: #FFFFFF;
   opacity: 0.1;
   border-radius: 50%;
   cursor: pointer;
   transform: scale(1);
-  transition: transform 0.5s;
+  transition: transform 0.3s;
   &:hover {
     transform: scale(calc(450 / 274));
   }
 `
-
-const LabelBox = styled.div`
-  height: 274px;
-  width: 274px;
-  position: absolute;
-  right: 165px;
-  bottom: 131px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  pointer-events: none;
-`
-
 const CircleLabel = styled.span`
-  font-size: 32px;
-  letter-spacing: 2px;
-  color: #0B0D17;
+  @media (max-width: ${tabletToMobile}px) {
+    position: absolute;
+    height: 23px;
+    left: 19.67%;
+    right: 18.33%;
+    top: calc(50% - 23px/2 + 4.5px);
+
+    font-family: Bellefair;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 23px;
+    /* identical to box height */
+
+    text-align: center;
+    letter-spacing: 1.25px;
+
+    color: #0B0D17;
+  }
+  @media (min-width: ${tabletToMobile}px) and (max-width: ${desktopToTablet}px) {
+    position: absolute;
+    height: 37px;
+    left: 19.01%;
+    right: 19.83%;
+    top: calc(50% - 37px/2 + 0.5px);
+
+    font-family: Bellefair;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 32px;
+    line-height: 37px;
+    /* identical to box height */
+
+    letter-spacing: 2px;
+
+    color: #0B0D17;
+  }
+  @media (min-width: ${desktopToTablet}px) {
+    position: absolute;
+    height: 37px;
+    left: 22.63%;
+    right: 23.36%;
+    top: calc(50% - 37px/2 + 0.5px);
+
+    font-family: Bellefair;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 32px;
+    line-height: 37px;
+    /* identical to box height */
+
+    letter-spacing: 2px;
+
+    color: #0B0D17;
+  }
 `;
 
 const Home: NextPage = () => {
   return (
     <>
       <TextBox>
-        <H2 className="font-secondary">{pre}</H2>
+        <H2>{pre}</H2>
         <H1>{title}</H1>
-        <P className="font-body">{description}</P>
+        <P>{description}</P>
       </TextBox>
-      <Circle />
-      <Link href="/destination">
-        <CircleTransparent />
-      </Link>
-      <LabelBox>
+      <CircleContainer>
+        <Circle />
+        <Link href="/destination">
+          <CircleTransparent />
+        </Link>
         <CircleLabel>
           EXPLORE
         </CircleLabel>
-      </LabelBox>
+      </CircleContainer>
     </>
   )
 }
